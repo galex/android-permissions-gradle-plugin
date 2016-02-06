06d832dc1803ee7b1d998bc4bf4d5de6622e92b1
Alexander Gherschon <alexandre.gherschon@gmail.com>
2016-02-06 11:42:01 +0200

    Initial import

---
+++ src/main/groovy/co/il/galex/permissions/PermissionsPlugin.groovy
@@ -0,0 +1,11 @@
+package co.il.galex.permissions;
+
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class PermissionsPlugin implements Plugin<Project> {
+    @Override void apply(Project project) {
+
+    }
+}
